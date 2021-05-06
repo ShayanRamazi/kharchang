af1c9149e50756ade0e3366c9f7e3cdd1aba4978
hamed <vasei.hamed@gmail.com>
2021-05-06 19:13:37 +0430

    Bug fix in logging and selecting dates

--- tsetmc/tasks.py
+++ tsetmc/tasks.py
@@ -31,37 +31,45 @@ def tsetmc_daily_crawl():
     tse_instrument_id_list = get_instrument_id_list_from_tsetmc()
     for instrument_id in tse_instrument_id_list:
         if task_counter > DAILY_TSE_CRAWL_LIMIT_IN_EACH_RUN:
-            logger.info("More than" + str(DAILY_TSE_CRAWL_LIMIT_IN_EACH_RUN) +  "tasks added! I think that's enough for this time!")
+            logger.info("More than" + str(
+                DAILY_TSE_CRAWL_LIMIT_IN_EACH_RUN) + "tasks added! I think that's enough for this time!")
             return
         logger.info("Get date list for " + str(instrument_id))
         # get dates to crawl for each instrument in descending order
         dates_list = get_instrument_dates_to_crawl(instrument_id)
         if len(dates_list) == 0:
-            logger.info("empty dates list to crawl for id:", instrument_id)
+            logger.info("empty dates list to crawl for id:" + instrument_id)
             continue
         # get all tsetmc_crawl_tasks for this instrument with state D and Q ordered by date descending
         logger.info("query database for " + str(instrument_id))
-        all_d_q_tse_tasks_for_instrument = TseTmcCrawlTask.objects.filter(
+        all_d_q_tse_tasks_dates_for_instrument = TseTmcCrawlTask.objects.filter(
             instrumentId=instrument_id,
             state__in=['Q', 'D']) \
-            .order_by('-dateToCrawl')
+            .values('dateToCrawl')
 
-        dates_list_index = 0
         logger.info("adding tasks")
-        for task in all_d_q_tse_tasks_for_instrument:
-            temp_date = dates_list[dates_list_index]
-            while temp_date >= task.dateToCrawl:
-                if temp_date != task.dateToCrawl:
-                    task_counter += 1
-                    add_tsetmc_task(instrument_id, temp_date)
-                dates_list_index += 1
-                if dates_list_index >= len(dates_list):
-                    break
-                temp_date = dates_list[dates_list_index]
+        # added_dates = [task.dateToCrawl for task in all_d_q_tse_tasks_for_instrument]
+        dates_to_add = [x for x in dates_list if x not in all_d_q_tse_tasks_dates_for_instrument]
+        for date_to_add in dates_to_add:
+            add_tsetmc_task(instrument_id, date_to_add)
+        task_counter += len(dates_to_add)
 
-        for i in range(dates_list_index, len(dates_list)):
-            task_counter += 1
-            add_tsetmc_task(instrument_id, dates_list[i])
+        # dates_list_index = 0
+        # for task in all_d_q_tse_tasks_for_instrument:
+        #     temp_date = dates_list[dates_list_index]
+        #     while temp_date >= task.dateToCrawl:
+        #         if temp_date != task.dateToCrawl:
+        #             task_counter += 1
+        #             add_tsetmc_task(instrument_id, temp_date)
+        #         dates_list_index += 1
+        #         if dates_list_index >= len(dates_list):
+        #             break
+        #         temp_date = dates_list[dates_list_index]
+        #
+        # for i in range(dates_list_index, len(dates_list)):
+        #     task_counter += 1
+        #     add_tsetmc_task(instrument_id, dates_list[i])
+        #
 
 
 def add_tsetmc_task(instrument_id, date):
@@ -125,7 +133,6 @@ def add_single_client_type_data(client_type_string, time_iso_format):
     client_type_data.volumeSellLegal = parts[8]
     client_type_data.save()
 
-
 # @transaction.atomic()
 # def remove_redundant_client_type_records(instrumentId, date):
 #     all_records = MiddleDayClientTypeData.objects.filter(instrumentId=instrumentId, date=date).sort()
@@ -133,4 +140,3 @@ def add_single_client_type_data(client_type_string, time_iso_format):
 #     ids_to_delete
 #     for record in all_records:
 #         record_string =
-
